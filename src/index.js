d7e50aff76cd4d91a3350ef54c37c1a076fbb235
bobrovice <kirill.madze@gmail.com>
2019-10-31 16:13:27 +0300

    Added range slider

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ require('./common.blocks/input-date');
 require('./common.blocks/expandable-list');
 require('./common.blocks/like-button');
 require('./common.blocks/rate-button');
+require('./common.blocks/range-slider');
